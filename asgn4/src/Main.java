21d883018e90bce896a654a84932b0b9da92a61c
Caleb Kumar <kumarcal@Calebs-MacBook-Air.local>
2017-09-30 10:41:52 -0500

    read test case 1 in properly

--- asgn4/src/Main.java
+++ asgn4/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
         Scratchpad managementConsole = new Scratchpad();
-
-        String[] managementSystemFiles = {"courses.csv", "instructors.csv", "listings.csv", "prereqs.csv", "programs.csv", "records.csv", "requests.csv", "students.csv"};
+//        System.out.println(System.getProperty("user.dir"));
+        String[] managementSystemFiles = {"test/courses.csv", "test/instructors.csv", "test/listings.csv", "test/prereqs.csv", "test/programs.csv", "test/records.csv", "test/requests.csv", "test/students.csv"};
         for (String nextFileName : managementSystemFiles) {
             managementConsole.uploadFileContents(nextFileName);
         }
--- asgn4/src/edu/gatech/Scratchpad.java
+++ asgn4/src/edu/gatech/Scratchpad.java
@@ -5,48 +5,49 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Scratchpad {
-		
-	public Scratchpad() { }
+
+    public Scratchpad() {
+    }
 
     private void processFileContents(String inputFileName, String[] tokens) {
-		//TODO REMOVE THESE PRINT STATEMENTS BEFORE SUBMISSION
+        //TODO REMOVE THESE PRINT STATEMENTS BEFORE SUBMISSION
         // display the contents (token) in the line selected for processing
         switch (inputFileName) {
-        case "courses.csv":
-    			System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2]);
-    			break;
-        case "instructors.csv":
-        		System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2] + ", " + tokens[3] + ", " + tokens[4]);
-        		break;
-    		case "listings.csv":
-    			System.out.println(tokens[0] + ", " + tokens[1]);
-    			break;
-		case "prereqs.csv":
-    			System.out.println(tokens[0] + ", " + tokens[1]);
-    			break;
-		case "programs.csv":
-    			System.out.println(tokens[0] + ", " + tokens[1]);
-    			break;
-		case "records.csv":
-    			System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2] + ", " + tokens[3] + ", " + tokens[4]);
-    			break;
-		case "requests.csv":
-    			System.out.println(tokens[0] + ", " + tokens[1]);
-    			break;
-		case "students.csv":
-            	System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2] + ", " + tokens[3] + ", " + tokens[4]);
-            	break;
-    		default:
-    			System.out.println("# error: unknown input file name");
-    			break;
+            case "test/courses.csv":
+                System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2]);
+                break;
+            case "test/instructors.csv":
+                System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2] + ", " + tokens[3] + ", " + tokens[4]);
+                break;
+            case "test/listings.csv":
+                System.out.println(tokens[0] + ", " + tokens[1]);
+                break;
+            case "test/prereqs.csv":
+                System.out.println(tokens[0] + ", " + tokens[1]);
+                break;
+            case "test/programs.csv":
+                System.out.println(tokens[0] + ", " + tokens[1]);
+                break;
+            case "test/records.csv":
+                System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2] + ", " + tokens[3] + ", " + tokens[4]);
+                break;
+            case "test/requests.csv":
+                System.out.println(tokens[0] + ", " + tokens[1]);
+                break;
+            case "test/students.csv":
+                System.out.println(tokens[0] + ", " + tokens[1] + ", " + tokens[2] + ", " + tokens[3] + ", " + tokens[4]);
+                break;
+            default:
+                System.out.println("# error: unknown input file name");
+                break;
         }
     }
-        
+
     public void uploadFileContents(String inputFileName) {
-    		// REMOVE THIS PRINT STATEMENT BEFORE SUBMISSION
-    		// display the name of the file selected for processing
-    		System.out.println(inputFileName);
-    		
+        //TODO REMOVE THESE PRINT STATEMENTS BEFORE SUBMISSION
+        // display the name of the file selected for processing
+        System.out.println(inputFileName);
+
         // Input file which needs to be parsed
         String fileToParse = inputFileName;
         BufferedReader fileReader = null;
@@ -75,5 +76,5 @@ public class Scratchpad {
             }
         }
     }
-    
+
 }
